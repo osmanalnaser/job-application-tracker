3082c958e1a433d5f7f893e6bd055bf931e172db
osmanalnaser <osman.n2@icloud.com>
2026-03-16 13:51:49 +0100

    feat: add login service logic

--- backend/src/main/java/com/osmanalnaser/job_application_tracker/auth/AuthService.java
+++ backend/src/main/java/com/osmanalnaser/job_application_tracker/auth/AuthService.java
@@ -33,4 +33,15 @@ public class AuthService {
 
         userRepository.save(user);
     }
+
+    public void login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials"));
+
+        boolean passwordMatches = passwordEncoder.matches(request.getPassword(), user.getPassword());
+
+        if (!passwordMatches) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
+        }
+    }
 }
