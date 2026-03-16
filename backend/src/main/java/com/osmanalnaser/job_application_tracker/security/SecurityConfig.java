cf837828d4c5b8f043c2c965f5968f1bf33b115d
osmanalnaser <osman.n2@icloud.com>
2026-03-16 13:31:29 +0100

    feat: add pasword encoder bean

--- backend/src/main/java/com/osmanalnaser/job_application_tracker/security/SecurityConfig.java
+++ backend/src/main/java/com/osmanalnaser/job_application_tracker/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.osmanalnaser.job_application_tracker.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,6 +13,12 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .httpBasic(httpBasic -> httpBasic.disable())
                 .build();
+    }
+
+    @Bean
+    public org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
+        return new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
     }
 }
