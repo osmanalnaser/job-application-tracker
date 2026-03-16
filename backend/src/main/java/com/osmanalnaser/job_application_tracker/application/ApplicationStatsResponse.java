f82ee98ddb80ca57a771b01f342c49f7cd480524
osmanalnaser <osman.n2@icloud.com>
2026-03-16 12:24:30 +0100

    feat: add application stats response dto

---
+++ backend/src/main/java/com/osmanalnaser/job_application_tracker/application/ApplicationStatsResponse.java
@@ -0,0 +1,44 @@
+package com.osmanalnaser.job_application_tracker.application;
+
+public class ApplicationStatsResponse {
+
+    private long applied;
+    private long interview;
+    private long rejected;
+    private long offer;
+
+    public ApplicationStatsResponse() {
+    }
+
+    public long getApplied() {
+        return applied;
+    }
+
+    public void setApplied(long applied) {
+        this.applied = applied;
+    }
+
+    public long getInterview() {
+        return interview;
+    }
+
+    public void setInterview(long interview) {
+        this.interview = interview;
+    }
+
+    public long getRejected() {
+        return rejected;
+    }
+
+    public void setRejected(long rejected) {
+        this.rejected = rejected;
+    }
+
+    public long getOffer() {
+        return offer;
+    }
+
+    public void setOffer(long offer) {
+        this.offer = offer;
+    }
+}
