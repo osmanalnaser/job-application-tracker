82d143d408dac69c157db465d5bf6b0d73cc738d
osmanalnaser <osman.n2@icloud.com>
2026-03-16 14:26:00 +0100

    feat: add jwt service

---
+++ backend/src/main/java/com/osmanalnaser/job_application_tracker/security/JwtService.java
@@ -0,0 +1,30 @@
+package com.osmanalnaser.job_application_tracker.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 Stunde
+
+    public String generateToken(String email) {
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+}
