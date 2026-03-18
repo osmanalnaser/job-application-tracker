32ddf6c682aa4bec03fc0586bcf098942d142629
osmanalnaser <osman.n2@icloud.com>
2026-03-18 10:40:54 +0100

    feat: add basic page components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<div>Home</div>} />
-        <Route path="/login" element={<div>Login Page</div>} />
-        <Route path="/register" element={<div>Register Page</div>} />
-        <Route path="/dashboard" element={<div>Dashboard Page</div>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/dashboard" element={<DashboardPage/>} />
       </Routes>
     </BrowserRouter>
   );
