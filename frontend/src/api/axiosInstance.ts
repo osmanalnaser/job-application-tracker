89a1e172ee3ac1448ce9acd8237622b596ae4402
osmanalnaser <osman.n2@icloud.com>
2026-04-14 22:33:25 +0200

    feat: add backend url

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://job-application-tracker-31lr.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use(
